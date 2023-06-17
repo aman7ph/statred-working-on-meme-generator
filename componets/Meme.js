f98ed3b7e166826477ca7b14b057b49226636fca
amanph <122424467+amanph@users.noreply.github.com>
2023-06-17 10:33:00 +0300

    applied state for changing img url

--- componets/Meme.js
+++ componets/Meme.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import memesData from './../data/memeData.js';
 const Meme = () => {
-  const [memeimage, memeimageState] = React.useState(
-    'https://i.imgflip.com/1x6f.jpg'
-  );
+  const [all_meme_image, setAllmemeimage] = React.useState(memesData);
+  const [meme, setmeme] = React.useState({
+    upper_caption: '',
+    bottom_caption: '',
+    random_meme_image: 'https://i.imgflip.com/1x6f.jpg',
+  });
+
   const randomMemeImage = () => {
-    const memeArr = memesData.data.memes;
-    memeimageState(memeArr[Math.floor(Math.random() * memeArr.length)].url);
+    const meme_arr = all_meme_image.data.memes;
+    const rnad_num = Math.floor(Math.random() * meme_arr.length);
+    setmeme((prev_meme) => ({
+      ...prev_meme,
+      random_meme_image: meme_arr[rnad_num].url,
+    }));
   };
   return (
     <main>
@@ -17,7 +25,7 @@ const Meme = () => {
           Add caption
         </button>
       </div>
-      <img src={memeimage} alt="meme_image_tobe_captioned" />
+      <img src={meme.random_meme_image} alt="meme_image_tobe_captioned" />
     </main>
   );
 };
