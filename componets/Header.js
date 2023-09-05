39b32b01151468e32d19b3a94365652318417c87
amanph <122424467+amanph@users.noreply.github.com>
2023-09-05 08:49:24 +0300

    almost done

--- componets/Header.js
+++ componets/Header.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-const Header=()=>{
-  return(
-    <div class='header'>
-      <img src="https://static.thenounproject.com/png/105260-200.png" alt="meme-logo"/>
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        src="https://static.thenounproject.com/png/105260-200.png"
+        alt="meme-logo"
+      />
       <h1>Best MeMes</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- componets/Meme.js
+++ componets/Meme.js
@@ -1,31 +1,63 @@
 import React from 'react';
-import memesData from './../data/memeData.js';
+//mport memesData from './../data/memeData.js';
 const Meme = () => {
-  const [all_meme_image, setAllmemeimage] = React.useState(memesData);
+  const [all_memes, setAllmemeimage] = React.useState([]);
   const [meme, setmeme] = React.useState({
     upper_caption: '',
     bottom_caption: '',
     random_meme_image: 'https://i.imgflip.com/1x6f.jpg',
   });
-
+  React.useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+      .then((res) => res.json())
+      .then((data) => setAllmemeimage(data.data.memes));
+  }, []);
   const randomMemeImage = () => {
-    const meme_arr = all_meme_image.data.memes;
-    const rnad_num = Math.floor(Math.random() * meme_arr.length);
+    const rnad_num = Math.floor(Math.random() * all_memes.length);
     setmeme((prev_meme) => ({
       ...prev_meme,
-      random_meme_image: meme_arr[rnad_num].url,
+      random_meme_image: all_memes[rnad_num].url,
     }));
   };
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setmeme((prevCaption) => ({
+      ...prevCaption,
+      [name]: value,
+    }));
+  }
   return (
     <main>
       <div className="caption_adder">
-        <input type="text" placeholder="upper caption" className="Minput" />
-        <input type="text" placeholder="Bottom text" className="Minput" />
+        <input
+          type="text"
+          placeholder="upper caption"
+          className="Minput"
+          onChange={handleChange}
+          name="upper_caption"
+          value={meme.upper_caption}
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="Minput"
+          onChange={handleChange}
+          name="bottom_caption"
+          value={meme.bottom_caption}
+        />
         <button className="btn" onClick={randomMemeImage}>
           Add caption
         </button>
       </div>
-      <img src={meme.random_meme_image} alt="meme_image_tobe_captioned" />
+      <div className="meme">
+        <img
+          src={meme.random_meme_image}
+          alt="meme_image_tobe_captioned"
+          className="meme--image"
+        />
+        <h2 className="meme--text top">{meme.upper_caption}</h2>
+        <h2 className="meme--text bottom">{meme.bottom_caption}</h2>
+      </div>
     </main>
   );
 };
